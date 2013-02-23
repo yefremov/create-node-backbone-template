51a6d7088e9c20dea4b491fe051578d2d9f19bd4
Anton Yefremov <anton.yefremov@gmail.com>
2013-02-23 18:08:26 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,8 @@ var path    = require( 'path' )
   , express = require( 'express' )
   , app     = express();
 
-var rootDir = __dirname
-  , wwwDir  = path.join( rootDir, 'www' );
-
 app.configure(function () {
-  app.use( express.static(wwwDir) );
+  app.use( express.static( path.join(__dirname, 'www')) );
 });
 
 app.listen( 3000 );
